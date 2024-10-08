7e2c3540b1c3762c0a8be32fa9e490e9f10df0e4
alope0420@oslomet.no <alexander@opedal.org>
2024-10-08 12:09:19 +0200

    La til lyd og blinkende knapper

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const tilstandsdefinisjoner= {
     OK: {
         led: 'green',
         button: 'off',
-        tekst: 'Status for røykvarsler: OK<br>Linje 2<br>Linje 3<br>Linje 4',
+        tekst: 'Status for røykvarsler: OK',
     },
     UTLØST: {
         led: 'red',
         button: 'red',
+        blinkende: true,
+        lyd: '1_advarsel_loop.wav',
+        gjentaLyd: true,
         nedtelling: 120,
         tilstandEtterNedtelling: 'TILKALT',
         tekst: 'Røykvarsler utløst<br>' +
@@ -16,6 +19,9 @@ const tilstandsdefinisjoner= {
     SLUMRET: {
         led: 'yellow',
         button: 'yellow',
+        blinkende: true,
+        lyd: '2_alarm_kvittert_loop.wav',
+        gjentaLyd: true,
         nedtelling: 300,
         tilstandEtterNedtelling: 'TILKALT',
         tekst: 'Vennligst luft ut<br>' +
@@ -25,13 +31,16 @@ const tilstandsdefinisjoner= {
     TILKALT: {
         led: 'red',
         button: 'off',
+        lyd: '4_utløpt_tid_loop.wav',
+        gjentaLyd: true,
         tekst: 'Varsel er sendt til brannsentral<br>' +
             'Venter på utrykning',
     },
     NULLSTILT: {
         led: 'green',
         button: 'green',
-        nedtelling: 5,
+        lyd: '5_røyken_er_borte.wav',
+        nedtelling: 7,
         tilstandEtterNedtelling: 'OK',
         tekst: 'Ingen røyk registrert<br>' +
             'Alarm avbrutt <img id="hakemerke" src="assets/checkmark.gif" alt="">',
@@ -41,33 +50,58 @@ const tilstandsdefinisjoner= {
 
 let nåværendeTilstand = 'OK';
 const utløsAlarmEtter = 5;  // i sekunder
-const nedtellingsekunder = 300;
-
+const blinkHastighet = 750; // i millisekunder
+const innlastingstid = Date.now();
 let nedtelling;
+
+let blinkIntervall;
+
 
 function settTilstand(tilstand) {
 
-    //console.log(`Tilstand endret til ${tilstand} etter)
+    console.log(`Tilstand endret til ${tilstand} etter ${Date.now() - innlastingstid} ms`);
 
     nåværendeTilstand = tilstand;
     const definisjon = tilstandsdefinisjoner[tilstand];
-    $('#knapp-bilde').attr('src', `assets/button_${definisjon.button}.png`);
-    $('#led-bilde').attr('src', `assets/LED_${definisjon.led}.png`);
+
+    let alternativ = false;
+    const byttGrafikk = () => {
+        if (!alternativ) {
+            $('#knapp-bilde').attr('src', `assets/button_${definisjon.button}.png`);
+            $('#led-bilde').attr('src', `assets/LED_${definisjon.led}.png`);
+        } else {
+            $('#knapp-bilde').attr('src', `assets/button_off.png`);
+            $('#led-bilde').attr('src', ``);
+        }
+        alternativ = !alternativ;
+    }
+    clearInterval(blinkIntervall);
+    if (definisjon.blinkende) {
+        blinkIntervall = setInterval(byttGrafikk, blinkHastighet);
+    }
+    byttGrafikk();
     $('#display').html(definisjon.tekst);
 
-    clearTimeout(nedtelling);
+    $('#tts-lyd').attr('src', `audio/${definisjon.lyd}`);
+    if (definisjon.gjentaLyd)
+        $('#tts-lyd').prop('loop', true);
+    else
+        $('#tts-lyd').removeProp('loop');
+    $('#tts-lyd')[0].play();
+
+    clearInterval(nedtelling);
 
     if (definisjon.nedtelling) {
         let sekunder = definisjon.nedtelling;
         const oppdaterDisplay = () => {
+            if (sekunder < 0) {
+                clearInterval(nedtelling);
+                settTilstand(definisjon.tilstandEtterNedtelling);
+            }
             $('#nedtelling').text(
                 Math.floor(sekunder / 60) + ':' +
                 ('' + Math.floor(sekunder % 60)).padStart(2, '0'));
             --sekunder;
-            if (!sekunder) {
-                clearTimeout(nedtelling);
-                settTilstand(definisjon.tilstandEtterNedtelling);
-            }
         }
         nedtelling = setInterval(oppdaterDisplay, 1000);
         oppdaterDisplay();
@@ -77,7 +111,11 @@ function settTilstand(tilstand) {
 function trykkKnapp() {
     switch (nåværendeTilstand) {
         case 'OK': {
-            settTilstand('UTLØST');
+            clearTimeout(nedtelling);
+            nedtelling = setTimeout(() => {
+                settTilstand('UTLØST');
+                $('audio')[0].play();
+            }, utløsAlarmEtter * 1000);
             break;
         }
         case 'UTLØST': {
@@ -96,9 +134,6 @@ function trykkKnapp() {
     }
 }
 
-const innlastingstid = Date.now();
-
 $(document).ready(() => {
     settTilstand('OK');
-    setTimeout(() => settTilstand('UTLØST'), utløsAlarmEtter * 1000)
 })
